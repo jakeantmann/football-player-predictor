5ea45f2bf34a3b82bce559338c1d7d81e59e085b
Jake Antmann <jakeantmann@gmail.com>
2021-08-11 14:28:57 +1000

    Initialise project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the future potential of up and coming footballers with Machine Learning.',
+    author='Jake Antmann',
+    license='BSD-3',
+)
